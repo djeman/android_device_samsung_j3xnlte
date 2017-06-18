0f407abe9a4a064f453275885f1b245781f5976d
adrian1234a <adri16072002@gmail.com>
2017-06-18 18:29:00 +0200

    Make compatible with lineage

--- lineage.mk
+++ lineage.mk
@@ -12,7 +12,7 @@ $(call inherit-product, device/samsung/j3xnlte/device_j3xnlte.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := j3xnlte
-PRODUCT_NAME := cm_j3xnlte
+PRODUCT_NAME := lineage_j3xnlte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-J320FN
 PRODUCT_MANUFACTURER := samsung
