8c8ffff72f388de1fd6eea0d128f2d4ccff9a592
adrian1234a <adri16072002@gmail.com>
2017-07-13 17:12:34 +0200

    Add FingerPrint And Info Of Device

--- lineage.mk
+++ lineage.mk
@@ -16,3 +16,8 @@ PRODUCT_NAME := lineage_j3xnlte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-J320FN
 PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="Samsung Galaxy J3(6)" \
+    BUILD_FINGERPRINT=samsung/j3xnltexx/j3xnlte:5.1.1/LMY47V/J320FNXXU0APH1:user/release-keys \
+    PRIVATE_BUILD_DESC="j3xnltexx-user 5.1.1 LMY47V J320FNXXU0APH1 release-keys"
