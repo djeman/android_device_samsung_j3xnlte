376461febd7c5c40123c26178c3a790ae40a1560
Danijel Jovanovic <30354085+danijelj2001@users.noreply.github.com>
2018-01-15 14:25:27 +0100

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -23,7 +23,7 @@
 # *not* include it on all devices, so it is safe even with hardware-specific
 # components.
 
-ifneq ($(filter j3xnlte,$(TARGET_DEVICE)),)
+ifneq ($(filter j2xlte,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
