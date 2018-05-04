5039c46efcd2289ceffc9fac05c8cb34a85da0c1
djeman <djemouli@gmail.com>
2018-05-04 17:56:36 +0200

    gps: update for los 15.1
    Change-Id: I7384382e3adfacb20adf2c1d02fbde07cac225ce

--- gps/device-gps.mk
+++ gps/device-gps.mk
@@ -2,9 +2,9 @@
 LOCAL_PATH := device/samsung/j3xnlte/gps
 
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/gpsd:system/bin/gpsd \
-	$(LOCAL_PATH)/gps.default.so:system/lib/hw/gps.default.so \
-	$(LOCAL_PATH)/gps.xml:system/etc/gps.xml \
-	$(LOCAL_PATH)/libwrappergps.so:system/lib/libwrappergps.so \
-	$(LOCAL_PATH)/gps.cer:system/bin/gps.cer
+	$(LOCAL_PATH)/gpsd:$(TARGET_COPY_OUT_VENDOR)/bin/gpsd \
+	$(LOCAL_PATH)/gps.default.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/gps.default.so \
+	$(LOCAL_PATH)/gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/gps.xml \
+	$(LOCAL_PATH)/libwrappergps.so:$(TARGET_COPY_OUT_VENDOR)/lib/libwrappergps.so \
+	$(LOCAL_PATH)/gps.cer:$(TARGET_COPY_OUT_VENDOR)/bin/gps.cer
 
