08a4bd7e543a6e2cbb044c782035675aa9b6046f
djeman <djemouli@gmail.com>
2018-09-30 01:11:54 +0200

    j3xnlte: move to common tree
    Change-Id: Idcfbea09795ba6485df55ceae67614c3f942da0d

--- lineage.mk
+++ lineage.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/j3xnlte/device.mk)
+$(call inherit-product, device/samsung/j3xnlte/j3xnlte.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := j3xnlte
